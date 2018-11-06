047671d187719bb9e533911e1dbcf4e71feb39de
Ryan B. Harvey <ryan.b.harvey@ted.com>
2018-11-06 15:20:59 -0600

    Add more tests for other filter argument formats

--- test/filter_node.test.js
+++ test/filter_node.test.js
@@ -8,7 +8,7 @@ let testFilter, otherTestFilter, badFilterDef1, badFilterDef2, badFilterDef3;
 describe('FilterNode', function () {
   describe('simple FilterNode objects', function () {
     this.beforeAll(() => {
-      // [tmp] crop=iw:ih/2:0:0 [cropped]
+      // basic test filter
       testFilter = {
         alias: 'cropFilter',
         options: {
@@ -35,6 +35,56 @@ describe('FilterNode', function () {
           toCommandArrayResult: ['[cropped]', 'vflip', '[flip]'],
           toCommandStringResult: '[cropped] vflip [flip]',
           toStringResult: 'FilterNode("vflipFilter", "[cropped] vflip [flip]")'
+        }
+      };
+      // array args
+      arrayArgsFilter = {
+        alias: 'aechoFilter',
+        options: {
+          filterName: 'aecho',
+          inputs: [{ alias: 'tmp'}],
+          outputs: [{ alias: 'echoed'}],
+          args: [0.8, 0.9, [1000, 1800], [0.3, 0.25]]
+        },
+        expectation: {
+          toCommandArrayResult: ['[tmp]', 'aecho=0.8:0.9:1000|1800:0.3|0.25', '[echoed]'],
+          toCommandStringResult: '[tmp] aecho=0.8:0.9:1000|1800:0.3|0.25 [echoed]',
+          toStringResult: 'FilterNode("aechoFilter", "[tmp] aecho=0.8:0.9:1000|1800:0.3|0.25 [echoed]")'
+        }
+      };
+      // keyword args
+      keywordArgsFilter = {
+        alias: 'cropKWFilter',
+        options: {
+          filterName: 'crop',
+          inputs: [{ alias: 'tmp'}],
+          outputs: [{ alias: 'cropped'}],
+          args: [
+            { name: "w", value: 100 },
+            { name: "h", value: 100 },
+            { name: "x", value: 12 },
+            { name: "y", value: 34 }
+          ]
+        },
+        expectation: {
+          toCommandArrayResult: ['[tmp]', 'crop=w=100:h=100:x=12:y=34', '[cropped]'],
+          toCommandStringResult: '[tmp] crop=w=100:h=100:x=12:y=34 [cropped]',
+          toStringResult: 'FilterNode("cropKWFilter", "[tmp] crop=w=100:h=100:x=12:y=34 [cropped]")'
+        }
+      };
+      // mixed args (this is horrible - don't do this - but we test anyway)
+      mixedArgsFilter = {
+        alias: 'cropMixedFilter',
+        options: {
+          filterName: 'crop',
+          inputs: [{ alias: 'tmp'}],
+          outputs: [{ alias: 'cropped'}],
+          args: [ { name: "x", value: 12 }, { name: "y", value: 34 }, 100, 100 ]
+        },
+        expectation: {
+          toCommandArrayResult: ['[tmp]', 'crop=100:100:x=12:y=34', '[cropped]'],
+          toCommandStringResult: '[tmp] crop=100:100:x=12:y=34 [cropped]',
+          toStringResult: 'FilterNode("cropMixedFilter", "[tmp] crop=100:100:x=12:y=34 [cropped]")'
         }
       };
       // no filterName
@@ -86,23 +136,63 @@ describe('FilterNode', function () {
       expect(f1.nextNode).to.eql(f2);
       expect(f2.previousNode).to.eql(f1);
     });
-    it('generates the correct command array', function () {
+    // array-only arguments
+    it('generates the correct command array for array-only arguments', function () {
       let f1 = new FilterNode(testFilter.alias, testFilter.options);
       expect(f1.toCommandArray()).to.deep.eql(testFilter.expectation.toCommandArrayResult);
       let f2 = new FilterNode(otherTestFilter.alias, otherTestFilter.options);
       expect(f2.toCommandArray()).to.deep.eql(otherTestFilter.expectation.toCommandArrayResult);
     });
-    it('generates the correct command string', function () {
+    it('generates the correct command string for array-only arguments', function () {
       let f1 = new FilterNode(testFilter.alias, testFilter.options);
       expect(f1.toCommandString()).to.deep.eql(testFilter.expectation.toCommandStringResult);
       let f2 = new FilterNode(otherTestFilter.alias, otherTestFilter.options);
       expect(f2.toCommandString()).to.deep.eql(otherTestFilter.expectation.toCommandStringResult);
     });
-    it('generates the correct string representation', function () {
+    it('generates the correct string representation for array-only arguments', function () {
       let f1 = new FilterNode(testFilter.alias, testFilter.options);
       expect(f1.toString()).to.deep.eql(testFilter.expectation.toStringResult);
       let f2 = new FilterNode(otherTestFilter.alias, otherTestFilter.options);
       expect(f2.toString()).to.deep.eql(otherTestFilter.expectation.toStringResult);
+    });
+    // array-valued arguments
+    it('generates the correct command array for array-valued arguments', function () {
+      let f = new FilterNode(arrayArgsFilter.alias, arrayArgsFilter.options);
+      expect(f.toCommandArray()).to.deep.eql(arrayArgsFilter.expectation.toCommandArrayResult);
+    });
+    it('generates the correct command string for array-valued arguments', function () {
+      let f = new FilterNode(arrayArgsFilter.alias, arrayArgsFilter.options);
+      expect(f.toCommandString()).to.deep.eql(arrayArgsFilter.expectation.toCommandStringResult);
+    });
+    it('generates the correct string representation for array-valued arguments', function () {
+      let f = new FilterNode(arrayArgsFilter.alias, arrayArgsFilter.options);
+      expect(f.toString()).to.deep.eql(arrayArgsFilter.expectation.toStringResult);
+    });
+    // keyword arguments
+    it('generates the correct command array for keyword arguments', function () {
+      let f = new FilterNode(keywordArgsFilter.alias, keywordArgsFilter.options);
+      expect(f.toCommandArray()).to.deep.eql(keywordArgsFilter.expectation.toCommandArrayResult);
+    });
+    it('generates the correct command string for keyword arguments', function () {
+      let f = new FilterNode(keywordArgsFilter.alias, keywordArgsFilter.options);
+      expect(f.toCommandString()).to.deep.eql(keywordArgsFilter.expectation.toCommandStringResult);
+    });
+    it('generates the correct string representation for keyword arguments', function () {
+      let f = new FilterNode(keywordArgsFilter.alias, keywordArgsFilter.options);
+      expect(f.toString()).to.deep.eql(keywordArgsFilter.expectation.toStringResult);
+    });
+    // mixed arguments
+    it('generates the correct command array for mixed arguments', function () {
+      let f = new FilterNode(mixedArgsFilter.alias, mixedArgsFilter.options);
+      expect(f.toCommandArray()).to.deep.eql(mixedArgsFilter.expectation.toCommandArrayResult);
+    });
+    it('generates the correct command string for mixed arguments', function () {
+      let f = new FilterNode(mixedArgsFilter.alias, mixedArgsFilter.options);
+      expect(f.toCommandString()).to.deep.eql(mixedArgsFilter.expectation.toCommandStringResult);
+    });
+    it('generates the correct string representation for mixed arguments', function () {
+      let f = new FilterNode(mixedArgsFilter.alias, mixedArgsFilter.options);
+      expect(f.toString()).to.deep.eql(mixedArgsFilter.expectation.toStringResult);
     });
   });
 });
