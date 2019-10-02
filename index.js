8a3d86091177cac623517a4b06e0bc431b137899
Ryan B. Harvey <ryan.b.harvey@ted.com>
2019-10-02 15:28:39 -0500

    Export FilterChain and FFmpegStreamSpecifier classes as library exports

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const getFessonia = (opts = {}) => {
     FFmpegInput: require('./lib/ffmpeg_input'),
     FFmpegOutput: require('./lib/ffmpeg_output'),
     FFmpegOption: require('./lib/ffmpeg_option'),
+    FFmpegStreamSpecifier: require('./lib/ffmpeg_stream_specifier'),
     FilterNode: require('./lib/filter_node'),
+    FilterChain: require('./lib/filter_chain'),
     FilterGraph: require('./lib/filter_graph'),
     FFmpegProgressEmitter: require('./lib/ffmpeg_progress_emitter'),
     Config: config
