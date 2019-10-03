dc48217e549220bff760ccea972e834bc091a660
Ryan B. Harvey <ryan.b.harvey@ted.com>
2019-10-03 10:17:29 -0500

    Remove Config from library exports

--- index.js
+++ index.js
@@ -4,14 +4,13 @@
  */
 
 const getFessonia = (opts = {}) => {
-  const config = require('./lib/util/config')(opts);
+  require('./lib/util/config')(opts);
   const Fessonia = {
     FFmpegCommand: require('./lib/ffmpeg_command'),
     FFmpegInput: require('./lib/ffmpeg_input'),
     FFmpegOutput: require('./lib/ffmpeg_output'),
     FilterNode: require('./lib/filter_node'),
-    FilterChain: require('./lib/filter_chain'),
-    Config: config
+    FilterChain: require('./lib/filter_chain')
   };
   return Fessonia;
 }
