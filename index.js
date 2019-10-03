e8598c0fff9cdc82f3ff6c4ecd24409e116d0be0
Ryan B. Harvey <ryan.b.harvey@ted.com>
2019-10-02 21:48:14 -0500

    Narrow the public API to only the public interface classes

--- index.js
+++ index.js
@@ -9,12 +9,8 @@ const getFessonia = (opts = {}) => {
     FFmpegCommand: require('./lib/ffmpeg_command'),
     FFmpegInput: require('./lib/ffmpeg_input'),
     FFmpegOutput: require('./lib/ffmpeg_output'),
-    FFmpegOption: require('./lib/ffmpeg_option'),
-    FFmpegStreamSpecifier: require('./lib/ffmpeg_stream_specifier'),
     FilterNode: require('./lib/filter_node'),
     FilterChain: require('./lib/filter_chain'),
-    FilterGraph: require('./lib/filter_graph'),
-    FFmpegProgressEmitter: require('./lib/ffmpeg_progress_emitter'),
     Config: config
   };
   return Fessonia;
