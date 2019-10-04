2b740f90389eeffb5870b012f6d3e5e494f23e38
Ryan B. Harvey <ryan.b.harvey@ted.com>
2019-10-04 15:46:54 -0500

    Cleanup jsdoc annotations

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 /**
  * @description "Fessonia"
+ * @fileOverview "Fessonia package import and configuration"
  * @author "Ryan B. Harvey <ryan.b.harvey@ted.com>"
  */
 
+/**
+ * Main function interface to the library. Returns object of classes when called.
+ * @param {Object} opts - an object of configuration options (@see Config)
+ * @returns {Object} - the library's interface classes: {@linkcode FFmpegCommand},  {@linkcode FFmpegInput},  {@linkcode FFmpegOutput},  {@linkcode FilterNode},  {@linkcode FilterChain}
+ */
 const getFessonia = (opts = {}) => {
   require('./lib/util/config')(opts);
   const Fessonia = {
